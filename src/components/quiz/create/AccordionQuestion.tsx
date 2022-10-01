3b048a9c2bae114d77cc41068218a7ab7e44ce0a
Javier Molpeceres <jamolpedev@gmail.com>
2022-10-01 23:08:21 +0200

    feat: accordion first status
    (cherry picked from commit 2131c9e5b0de0efc6208a054080c4214c24a13af)

---
+++ src/components/quiz/create/AccordionQuestion.tsx
@@ -0,0 +1,75 @@
+import { AccordionDetails, AccordionProps, Typography } from '@mui/material';
+import MuiAccordion from '@mui/material/Accordion';
+import { styled } from '@mui/material/styles';
+import MuiAccordionSummary, {
+  AccordionSummaryProps
+} from '@mui/material/AccordionSummary';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0
+  },
+  '&:before': {
+    display: 'none'
+  }
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)'
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1)
+  }
+}));
+
+type AccordionQuestionProps = {
+  panelName: string;
+  expanded: string | false;
+  handleChange: (
+    panel: string
+  ) => (event: React.SyntheticEvent, newExpanded: boolean) => void;
+};
+
+const AccordionQuestion = ({
+  panelName,
+  expanded,
+  handleChange
+}: AccordionQuestionProps) => {
+  return (
+    <div className="accordion">
+      <Accordion
+        expanded={expanded === panelName}
+        onChange={handleChange(panelName)}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Collapsible Group Item #1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default AccordionQuestion;
