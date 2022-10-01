3c1a6a545ff504f74ef918092b1c01e0a179dd2b
Javier Molpeceres <jamolpedev@gmail.com>
2022-10-01 22:27:07 +0200

    feat: title and add question button
    (cherry picked from commit f58688f86c7c241ffdaf8bc428131c8841f62984)

--- src/components/quiz/create/CreateForm.tsx
+++ src/components/quiz/create/CreateForm.tsx
@@ -1,6 +1,16 @@
+import { Button } from '@mui/material';
 import './CreateForm.scss';
 const CreateForm = () => {
-  return <div className="create-form">CreateForm</div>;
+  return (
+    <div className="create-form">
+      <div className="title-add">
+        <h1 className="title">Quiz</h1>
+        <div className="add-question">
+          <Button>Add question</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateForm;
