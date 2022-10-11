0eda88ebbf3609971eaeb712f26862001810fe09
Javier Molpeceres <jamolpedev@gmail.com>
2022-10-12 01:34:43 +0200

    feat: create question dropdown
    (cherry picked from commit 27407cef99be59aee37edb7c7c3b886f68d879eb)

--- src/components/quiz/create/CreateQuestion.tsx
+++ src/components/quiz/create/CreateQuestion.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
 import Formsy from 'formsy-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { QuestionType, questionTypes } from '../../../services/quiz/models';
 import DropDown from '../../common/DropDown';
 import TextInput from '../../common/TextInput';
@@ -11,32 +11,47 @@ type CreateQuestion = {
 };
 
 const CreateQuestion = ({ handleClose, createQuestion }: CreateQuestion) => {
+  const [createQuestionEnabled, setCreateQuestionEnabled] = useState(false);
   const createQuestionClick = (model: QuestionType) => {
     createQuestion(model);
     handleClose();
   };
   return (
-    <Formsy onSubmit={createQuestionClick}>
+    <Formsy
+      onValid={() => setCreateQuestionEnabled(true)}
+      onInvalid={() => setCreateQuestionEnabled(false)}
+      onSubmit={createQuestionClick}
+    >
       <div className="question-title">
         <TextInput
           required
+          validations={{ minLength: 5 }}
+          validationErrors={{
+            minLength: 'size should be more than 5 caracters'
+          }}
           name="title"
           label="Title"
           placeholder="Add your question title"
         />
-        <DropDown
-          required
-          id="question-type"
-          name="type"
-          label="type"
-          placeholder="Select your type question"
-          items={Object.keys(questionTypes).map((k) => ({
-            value: k,
-            label: questionTypes[k]
-          }))}
-        />
       </div>
-      <Button type="submit">Create</Button>
+      <DropDown
+        required
+        id="question-type"
+        name="type"
+        label="type"
+        placeholder="Select your type question"
+        items={Object.keys(questionTypes).map((k) => ({
+          value: k,
+          label: questionTypes[k]
+        }))}
+      />
+
+      {!createQuestionEnabled && (
+        <p className="error">Required fields must be filled</p>
+      )}
+      <Button disabled={!createQuestionEnabled} type="submit">
+        Create
+      </Button>
     </Formsy>
   );
 };
