72d4d07332675231fcf90705fc1af0f954ab0256
Javier Molpeceres <jamolpedev@gmail.com>
2022-10-12 16:12:12 +0200

    feat: type questions

---
+++ src/components/quiz/create/question/CheckTypeCreate.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CheckTypeCreate = () => {
+  return <div>CheckTypeCreate</div>;
+};
+
+export default CheckTypeCreate;
---
+++ src/components/quiz/create/question/TextTypeCreate.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextTypeCreate = () => {
+  return <div>TextTypeCreate</div>;
+};
+
+export default TextTypeCreate;
