9e38aef6f4b920ed2b1af931af3d0cb3062acf8d
Javier Molpeceres <jamolpedev@gmail.com>
2022-10-12 18:05:36 +0200

    fix: style

--- src/components/quiz/create/CreateQuiz.tsx
+++ src/components/quiz/create/CreateQuiz.tsx
@@ -10,11 +10,11 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  minWidth: 600,
   bgcolor: 'background.paper',
   border: '1px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 5,
   borderRadius: 5
 };
 
